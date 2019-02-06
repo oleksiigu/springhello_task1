8d86f9ade70492eb65de52f58f511f6aa63e098c
oleksiigu <OleksiiGunko@bitbucket.org>
2019-02-06 17:28:10 +0200

    added logic to open file on start of app and closing on destroy
    improved performance by reducing calls to fs

--- src/main/java/hellospring/task1/RndFileWriter.java
+++ src/main/java/hellospring/task1/RndFileWriter.java
@@ -4,28 +4,58 @@ import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @Component
 public class RndFileWriter {
 
     private static final String FILE_NAME = "rnd.txt";
+    private FileWriter fileWriter;
+    private boolean isFileOpened = false;
+
+    @PostConstruct
+    public void onInit(){
+        try{
+            fileWriter = new FileWriter(FILE_NAME);
+            isFileOpened = true;
+        }
+        catch (IOException exc){
+            System.out.println(String.format("RndFileWriter - IO Exception occurred: %s", exc.getMessage()));
+        }
+    }
 
     @Async
     @EventListener
     public void asyncHandleEvent(RndInfo event){
 
+        if(!isFileOpened){
+            System.out.println("File wasn't opened");
+            return;
+        }
+        
         try{
-            FileWriter fileWriter = new FileWriter(FILE_NAME, true);
-            try(PrintWriter printWriter = new PrintWriter(fileWriter)) {
-                printWriter.println(String.format("%s %s", event.getEventTime(), event.getValue()));
-            }
+            fileWriter.write(String.format("%s %s \n", event.getEventTime(), event.getValue()));
         }
         catch (IOException exc){
-            System.out.println(String.format("RndFileWriter - IO Exception occurred: %s", exc.getMessage()));
+            System.out.println(String.format("RndFileWriter - cant write to file: %s", exc.getMessage()));
         }
+    }
 
+
+    @PreDestroy
+    public void onDestroy(){
+        if(isFileOpened)
+        {
+            try {
+                fileWriter.flush();
+                fileWriter.close();
+            }
+            catch (IOException exc){
+                System.out.println(String.format("RndFileWriter - cant close a file: %s", exc.getMessage()));
+            }
+        }
     }
 }
